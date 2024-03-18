76ba5d275166bfd74cf1b347bba276d7c82cd383
Erryl <errylgalang@gmail.com>
2024-03-17 22:59:17 -0700

    Add spending_data collection

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -13,12 +13,21 @@ var uiConfig = {
                    window.location.assign("home.html");       
             }).catch(function (error) {
                    console.log("Error adding new user: " + error);
-            });
+            }).then(function () {
+              db.collection("users").doc(user.uid).collection("spending_data").doc().set({})
+                .then(function() {
+                  console.log("User document created with spending_data collection.");
+                })
+                .catch(function(error) {
+                  console.error("Error adding spending_data collection: ", error);
+                });
+            })
         } else {
             return true;
         }
             return false;
         },
+
       uiShown: function() {
 
         document.getElementById('loader').style.display = 'none';
