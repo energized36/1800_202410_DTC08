e5d2669c65bdfe4073f40ba8b962abc22fb04749
Erryl <errylgalang@gmail.com>
2024-03-25 05:40:38 -0700

    Add js for categories.html

--- scripts/script.js
+++ scripts/script.js
@@ -43,13 +43,12 @@ async function add_data(userID) {
     add();
 }
 
-function displayUserData(spendingData) {
-    // Clears the div to update it
-    $("#data_row").empty()
-    for (let key in spendingData) {
-        switch (spendingData[key].category) {
-            case "groceries":
-                category_icon = `<svg xmlns="http://www.w3.org/2000/svg"
+function getLogo(name) {
+    let categoryIcon;
+
+    switch (name) {
+        case "groceries":
+            categoryIcon = `<svg xmlns="http://www.w3.org/2000/svg"
                             class=" size-full icon icon-tabler icon-tabler-shopping-cart stroke-gold-main" width="44" height="44"
                             viewBox="0 0 24 24" stroke-width="2" fill="none" stroke-linecap="round"
                             stroke-linejoin="round">
@@ -59,9 +58,9 @@ function displayUserData(spendingData) {
                             <path d="M17 17h-11v-14h-2" />
                             <path d="M6 5l14 1l-1 7h-13" />
                             </svg>`
-                break;
-            case "food":
-                category_icon = `<svg xmlns="http://www.w3.org/2000/svg"
+            break;
+        case "food":
+            categoryIcon = `<svg xmlns="http://www.w3.org/2000/svg"
                 class=" size-full icon icon-tabler icon-tabler-tools-kitchen-2 stroke-gold-main" width="44" height="44"
                 viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round"
                 stroke-linejoin="round">
@@ -69,17 +68,17 @@ function displayUserData(spendingData) {
                 <path
                     d="M19 3v12h-5c-.023 -3.681 .184 -7.406 5 -12zm0 12v6h-1v-3m-10 -14v17m-3 -17v3a3 3 0 1 0 6 0v-3" />
             </svg>`
-                break;
-            case "clothing":
-                category_icon = ` <svg xmlns="http://www.w3.org/2000/svg" class="stroke-gold-main size-full icon icon-tabler icon-tabler-shirt"
+            break;
+        case "clothing":
+            categoryIcon = ` <svg xmlns="http://www.w3.org/2000/svg" class="stroke-gold-main size-full icon icon-tabler icon-tabler-shirt"
                 width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" fill="none"
                 stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M15 4l6 2v5h-3v8a1 1 0 0 1 -1 1h-10a1 1 0 0 1 -1 -1v-8h-3v-5l6 -2a3 3 0 0 0 6 0" />
             </svg>`
-                break;
-            case "self_care":
-                category_icon = `<svg xmlns="http://www.w3.org/2000/svg"
+            break;
+        case "self_care":
+            categoryIcon = `<svg xmlns="http://www.w3.org/2000/svg"
                             class=" size-full icon icon-tabler icon-tabler-mood-heart stroke-gold-main" width="44" height="44"
                             viewBox="0 0 24 24" stroke-width="2" fill="none" stroke-linecap="round"
                             stroke-linejoin="round">
@@ -90,9 +89,9 @@ function displayUserData(spendingData) {
                             <path d="M9.5 15a3.59 3.59 0 0 0 2.774 .99" />
                             <path d="M18.994 21.5l2.518 -2.58a1.74 1.74 0 0 0 .004 -2.413a1.627 1.627 0 0 0 -2.346 -.005l-.168 .172l-.168 -.172a1.627 1.627 0 0 0 -2.346 -.004a1.74 1.74 0 0 0 -.004 2.412l2.51 2.59z" />
                             </svg>`
-                break;
-            case "online_shopping":
-                category_icon = `<svg xmlns="http://www.w3.org/2000/svg"
+            break;
+        case "online_shopping":
+            categoryIcon = `<svg xmlns="http://www.w3.org/2000/svg"
                             class=" size-full icon icon-tabler icon-tabler-world-www stroke-gold-main" width="44" height="44"
                             viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round"
                             stroke-linejoin="round">
@@ -107,42 +106,103 @@ function displayUserData(spendingData) {
                             <path d="M17 10l1 4l1.5 -4l1.5 4l1 -4" />
                             <path d="M9.5 10l1 4l1.5 -4l1.5 4l1 -4" />
                             </svg>`
-                break;
-            case "school":
-                category_icon = `<svg xmlns="http://www.w3.org/2000/svg" class="stroke-gold-main size-full icon icon-tabler icon-tabler-school" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            break;
+        case "school":
+            categoryIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="stroke-gold-main size-full icon icon-tabler icon-tabler-school" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
                 <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
                 </svg>`
-                break;
-            default:
-                category_icon = "empty"
-                break;
-        }
+            break;
+        default:
+            categoryIcon = "empty"
+            break;
+    }
+    return categoryIcon
+}
 
+function displayUserData(spendingData, targetID, logo) {
+    console.log(targetID)
+    for (let key in spendingData) {
+        categoryIcon = getLogo(spendingData[key].category);
         if (spendingData[key].category != "empty") {
-            $("#data_row").append(`
+            if (logo) {
+                $(`#${targetID}`).append(`
             <div class="rounded-xl bg-white shadow-md mx-2 mt-2 flex items-center flex-col border">
                 <div class="h-[28px] w-full bg-green-sub rounded-t-xl my-auto px-2 text-white font-semibold">${spendingData[key].name}</div>
                 <div class="flex items-center justify-between w-full">
                     <div class="size-[40px] ">
-                        ${category_icon}
+                        ${categoryIcon}
                     </div>
                     <div class="font-inter font-bold flex text-md px-3 justify-between w-full text-2xl">
                         <div class="text-green-accent text-opacity-7">${spendingData[key].date}</div>
                         <div class="text-green-accent">$ ${spendingData[key].price}</div>
                     </div>
                 </div>
+            </div>`)
+            } else {
+                $(`#${targetID}`).append(`
+        <div class="rounded-xl bg-white shadow-md mx-2 mt-2 flex items-center flex-col border">
+            <div class="h-[28px] w-full bg-green-sub rounded-t-xl my-auto px-2 text-white font-semibold">${spendingData[key].name}</div>
+            <div class="flex items-center justify-between w-full">
+                <div class="font-inter font-bold flex text-md px-3 justify-between w-full text-2xl">
+                    <div class="text-green-accent text-opacity-7">${spendingData[key].date}</div>
+                    <div class="text-green-accent">$ ${spendingData[key].price}</div>
+                </div>
             </div>
-        `)
+        </div>`)
+            }
         }
     }
+}
+
+function displayCategories(spendingData) {
+    const categoryTotals = {};
+    var total = 0
+    spendingData.forEach(log => {
+        const category = log.category;
+        const price = log.price;
+        if (!categoryTotals[category]) {
+            categoryTotals[category] = 0;
+        }
+        total += parseFloat(price)
+        categoryTotals[category] += parseFloat(price);
+    });
+
+    const categoryResults = Object.keys(categoryTotals).map(category => {
+        const categoryTotal = categoryTotals[category];
+        const percentage = Math.round((categoryTotal / total) * 100);
+        return { category: category, total: categoryTotal, percentage: percentage };
+    });
+
+    categoryResults.sort((a, b) => b.total - a.total);
+
+    categoryResults.forEach(category => {
+        const adjustedPercentage = Math.max(category.percentage, 1);
+        $("#categories").append(`
+        <div id="${category.category}" class="mb-8">
+            <div class="flex justify-between mb-1 items-baseline">
+                <div class="size-16">${getLogo(category.category)}</div>
+                <p class="font-inter font-bold text-green-main text-xl">${adjustedPercentage}%</p>
+            </div>
+            <div class="w-full bg-gray-200 rounded-full h-8 bg-green-main overflow-clip">
+                <div class=" bg-green-accent h-8 rounded-full" style="width: ${adjustedPercentage}%"></div>
+            </div>
+            <div id="${category.category}-total" class="font-inter font-semibold text-green-main text-xl">${category.total.toFixed(2)}</div>
+            <div id="${category.category}-logs" class="max-h-[200px]"></div>
+        </div>`)
+
+        const categoryLogs = spendingData.filter(log => log.category === category.category);
+        displayUserData(categoryLogs, `${category.category}-logs`, false)
+    })
+
+
 }
 
 function queryUserTotal(userID) {
     db.collection("users").doc(userID).onSnapshot((doc) => {
         if (doc.exists) {
-            let total = doc.data().total || 0; 
+            let total = doc.data().total || 0;
             total = parseFloat(total).toFixed(2)
             $("#total").text(total)
         } else {
@@ -159,8 +219,10 @@ function queryUserData(userID) {
         snapshot.docs.forEach((doc) => {
             spendingData.push({ ...doc.data() });
         });
+        $(`#data_row`).empty()
         console.log(spendingData);
-        displayUserData(spendingData);
+        displayUserData(spendingData, "data_row", true);
+        displayCategories(spendingData);
     }, error => {
         console.error("Error getting spending data:", error);
     });
@@ -182,7 +244,7 @@ async function getUserID() {
 
 
 
-async function add_geolocation_btn_hander(){
+async function add_geolocation_btn_hander() {
     const user_id = await get_user_id();
     const longitude = $("#longitude_input").val();
     const lattitude = $("#lattitude_input").val();
@@ -197,8 +259,8 @@ async function add_geolocation_btn_hander(){
     display_geopoints()
 }
 
-async function display_geopoints(){
-    $("#geoResults").empty() 
+async function display_geopoints() {
+    $("#geoResults").empty()
     const user_id = await get_user_id();
 
     return await db.collection("users").doc(user_id)
@@ -206,12 +268,12 @@ async function display_geopoints(){
         .orderBy(`lat`)
         .get()
         .then(snapshot => {
-                snapshot.forEach(item => {
-                    // console.log(item.id)
-                    // console.log(item.data().long)
-                    // console.log(item.data().lat)
-                    // console.log(item.data())
-                    $("#geoResults").append(`
+            snapshot.forEach(item => {
+                // console.log(item.id)
+                // console.log(item.data().long)
+                // console.log(item.data().lat)
+                // console.log(item.data())
+                $("#geoResults").append(`
                         <div class="border flex gap-2 w-max-content">
                             <div class="flex gap-2">
                                 <label for="${item.id}"><strong>ID:</strong></label>
@@ -226,14 +288,15 @@ async function display_geopoints(){
                                 <div id="${item.id}">${item.data().lat}</div>
                             </div>
                         </div>
-                        `)})
-    })
+                        `)
+            })
+        })
 }
 
 async function setUp(userID) {
     queryUserData(userID);
     queryUserTotal(userID);
-    $("#dropdown").toggleClass("hidden");
+
     $("#Hamburger").on("click", hamburger_click_handler);
     $("#add").on("click", add);
     $("#desktop_add_btn").on("click", add);
@@ -246,7 +309,7 @@ async function setUp(userID) {
 }
 
 $("document").ready(() => {
-    getUserID().then( (userID) => {
+    getUserID().then((userID) => {
         setUp(userID)
     })
 });
@@ -256,76 +319,76 @@ $("document").ready(() => {
 
 const options = {
     chart: {
-      height: "100%",
-      maxWidth: "100%",
-      type: "area",
-      fontFamily: "Inter, sans-serif",
-      dropShadow: {
-        enabled: false,
-      },
-      toolbar: {
-        show: false,
-      },
+        height: "100%",
+        maxWidth: "100%",
+        type: "area",
+        fontFamily: "Inter, sans-serif",
+        dropShadow: {
+            enabled: false,
+        },
+        toolbar: {
+            show: false,
+        },
     },
     tooltip: {
-      enabled: true,
-      x: {
-        show: false,
-      },
+        enabled: true,
+        x: {
+            show: false,
+        },
     },
     fill: {
-      type: "gradient",
-      gradient: {
-        opacityFrom: 0.55,
-        opacityTo: 0,
-        shade: "#6DB423",
-        gradientToColors: ["#1C64F2"],
-      },
+        type: "gradient",
+        gradient: {
+            opacityFrom: 0.55,
+            opacityTo: 0,
+            shade: "#6DB423",
+            gradientToColors: ["#1C64F2"],
+        },
     },
     dataLabels: {
-      enabled: false,
+        enabled: false,
     },
     stroke: {
-      width: 6,
+        width: 6,
     },
     grid: {
-      show: false,
-      strokeDashArray: 4,
-      padding: {
-        left: 2,
-        right: 2,
-        top: 0
-      },
+        show: false,
+        strokeDashArray: 4,
+        padding: {
+            left: 2,
+            right: 2,
+            top: 0
+        },
     },
     series: [
-      {
-        name: "Nsew user",
-        data: [10, 6418, 6456, 6526, 6356, 6456],
-        color: "#4A9B30",
-      },
+        {
+            name: "Nsew user",
+            data: [10, 6418, 6456, 6526, 6356, 6456],
+            color: "#4A9B30",
+        },
     ],
     xaxis: {
-      categories: ['01 February', '02 February', '03 February', '04 February', '05 February', '06 February', '07 February'],
-      labels: {
-        show: false,
-      },
-      axisBorder: {
-        show: false,
-      },
-      axisTicks: {
-        show: false,
-      },
+        categories: ['01 February', '02 February', '03 February', '04 February', '05 February', '06 February', '07 February'],
+        labels: {
+            show: false,
+        },
+        axisBorder: {
+            show: false,
+        },
+        axisTicks: {
+            show: false,
+        },
     },
     yaxis: {
-      show: false,
+        show: false,
     },
-  }
-  
-  if (document.getElementById("area-chart") && typeof ApexCharts !== 'undefined') {
+}
+
+if (document.getElementById("area-chart") && typeof ApexCharts !== 'undefined') {
     const chart = new ApexCharts(document.getElementById("area-chart"), options);
     chart.render();
-  }
-  
+}
+
 
 
 
