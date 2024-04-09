ca6b5adbc89b39c7fc839498f6168ec31af92e4a
Erryl Galang <errylgalang@gmail.com>
2024-04-08 21:29:31 -0700

    Reset categories when cancelling

--- scripts/script.js
+++ scripts/script.js
@@ -93,6 +93,7 @@ function add() {
 function cancelAdd() {
     window.scrollTo(0, 0);
     console.log("Inside add function");
+    $("input[name='category']").prop("checked", false);
     $("#data_gui").toggleClass("collapse");
     $("#noCategorySelectedError").remove()
 }
@@ -194,14 +195,14 @@ async function addData(userID) {
     else {
         if ($("#noCategorySelectedError").length <= 0) {
             $("#data_gui_container").append(`
-                <div class='bg-white flex text-red-500 text-center items-center justify-center gap-1' id='noCategorySelectedError'>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alert-circle" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <div class='bg-white flex text-red-500 text-center items-center justify-center gap-1 font-inter' id='noCategorySelectedError'>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-alert-circle" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
                     <path d="M12 8v4" />
                     <path d="M12 16h.01" />
                     </svg>
-                    Please select a category
+                    Please select a category / input price
                 </div>`)
             $("#data_gui").toggleClass("animate-error")
             setTimeout(() => {
@@ -351,8 +352,6 @@ function displayUserData(spendingData, targetID, logo) {
     //         console.log("No checkbox checked");
     //     }
     // });
-
-
 }
 
 function getUserTotal(spendingData) {
